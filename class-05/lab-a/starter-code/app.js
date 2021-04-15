c88a049d110893f8d0d6603850afcc3e25f4a232
Raya22 <raya.lobany94@gmail.com>
2021-04-16 00:52:44 +0300

    task 4 had beeen done

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -78,15 +78,24 @@ IMPORTANT DETAIL: You may not use the arithmetic operator + in this function. To
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumArray() function and see if the test passes.*/
 
 // Write your code here
+
 let testArray = [2, 3, 4]; //eslint-disable-line
 
-function sumArray(sumArr) { //eslint-disable-line
+function sumArray(sumArr) {
+
+  let addFS=sum(sumArr[0],sumArr[1]);
+
+  let addFF=sum(addFS[0],sumArr[2]);
+  let final=[addFF[0],sumArr[0]+','+sumArr[1]+','+sumArr[2]+' was passed in as an array of numbers, and '+addFF[0]+' is their sum.'];
+  return final;
+
 
 }
+//console.log(sumArray(testArray));
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
